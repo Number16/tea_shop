92c5f9c7e06eb076cc5a45a591469f8feffeb510
Алексей Примечаев <primall96@mail.ru>
2017-03-14 01:52:25 +0300

    Сконфигурировал и добавил базовые классы, снабдив их необходимыми полями и свойствами.

---
+++ Tea Shop/Tea.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tea_Shop
+{
+    class Tea
+    {
+        private string _name;
+        private string _description;
+        private string _type;
+        private double _price;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Descriprion
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        public string Type
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
+
+        public double Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+
+        public Tea(string name, string description, string type, double price)
+        {
+            _name = name;
+            _description = description;
+            _type = type;
+            _price = price;
+        }
+
+    }
+}
---
+++ Tea Shop/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tea_Shop
+{
+    class User
+    {
+        private string _login;
+        private string _name;
+        private string _password;
+        private bool _hasEditAccess;
+
+        public string Login
+        {
+            get { return _login; }
+            set { _login = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+
+        public bool HasEditAccess
+        {
+            get { return _hasEditAccess; }
+            set { _hasEditAccess = value; }
+        }
+    }
+}
