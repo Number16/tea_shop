825242e91772e97353020976fe2885a960d1d892
Алексей Примечаев <primall96@mail.ru>
2017-03-14 22:49:22 +0300

    Добавлены и сконфигурированы (пока не функциональные окна для входа в приложение и регистрации пользователей).

--- Tea Shop/RegisterWindow.xaml.cs
+++ Tea Shop/RegisterWindow.xaml.cs
@@ -10,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Tea_Shop
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для RegisterWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class RegisterWindow : Window
     {
-        public MainWindow()
+        public RegisterWindow()
         {
             InitializeComponent();
-        }
-
-        private void button_Click(object sender, RoutedEventArgs e)
-        {
-            RegisterWindow regWin = new RegisterWindow();
-            regWin.Show();
-
         }
     }
 }
